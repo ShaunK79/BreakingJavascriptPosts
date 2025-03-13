b966c29b82cc243d8aa188368a3f308252c54bb1
ShaunK79 <91824973+ShaunK79@users.noreply.github.com>
2025-03-13 09:52:06 +0000

    Add files via upload

---
+++ arraysScript.js
@@ -0,0 +1,24 @@
+// Arrays
+
+// syntax
+const bikes = ['Honda', ' Suzuki', ' Yamaha', ' Aprilia', ' Kawasaki'];
+// document.write(bikes);
+// document.write(bikes.length); // 5
+// console.log(bikes[3]); // aprilia
+// console.log(bikes[0]);
+// bikes[3] = 'KTM';
+// console.log(bikes);
+console.log(bikes.indexOf('Yamaha'));
+
+
+// bikes = ['Honda', 'Suzuki', 'Yamaha'];
+// document.write(bikes);
+
+
+// //instead of doing this
+// let bike1 = 'Honda';
+// let bike2 = 'Suzuki';
+// let bike3 = 'Yamaha';
+
+
+
